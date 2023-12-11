ace3e29d0182c1f939666972c1838a9cb9de7acb
Wojciech Prusaczyk <woj.prusaczyk@gmail.com>
2023-12-11 12:56:53 +0100

    naprawa błędy menu bocznego, dostosowanie całości CMS do widoku z telefonów

--- assets/react/components/tile.jsx
+++ assets/react/components/tile.jsx
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 import {render} from "react-dom";
 
 const tile = (props) => {
-  return <a className={`tile${(props.isActive)?"":" disabled"}`} href={props.href}>
+  return <a className={`tile${(props.isActive)?"":" disabled"}`} href={`${location.protocol}//${window.location.host}/dashboard/${props.href}`}>
       <img className="tile-image" src={`/build/icons/dashboard/index/${props.icon}`} alt={`${props.name} icon`}/>
       <h2 className="tile-title">{props.name}</h2>
   </a>
